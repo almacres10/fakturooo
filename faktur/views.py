94025efc6507d81843c0f2ddcbb16674aa0161b9
alvin marco <alvinmarco8@gmail.com>
2023-11-10 15:47:25 +0700

    add tailwind and pagination

--- faktur/views.py
+++ faktur/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render, get_object_or_404
 from . models import Faktur2022
 from django.db.models import Q
 from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 
 
 
@@ -12,6 +14,22 @@ class DetailDataView(DetailView):
     template_name = 'faktur/detail.html'
     context_object_name = 'data'
 
+# def items(request):
+#     query = request.GET.get('query', '')
+#     items = Faktur2022.objects.all()
+
+#     if query:
+#         items = items.filter(Q(NAMA_PEMBELI__icontains=query))
+
+#     items_per_page = 10
+#     paginator = Paginator(items, items_per_page)
+#     page_number = request.GET.get('page')
+#     page_obj = paginator.get_page(page_number)
+
+#     return render(request, 'faktur/items.html', {
+#         "page_obj":page_obj
+#     })
+
 def items(request):
     query = request.GET.get('query', '')
     items = Faktur2022.objects.all()
@@ -19,7 +37,17 @@ def items(request):
     if query:
         items = items.filter(Q(NAMA_PEMBELI__icontains=query))
 
+    items_per_page = 10
+    paginator = Paginator(items, items_per_page)
+    
+    page_number = request.GET.get('page')
+    try:
+        page_obj = paginator.page(page_number)
+    except PageNotAnInteger:
+        page_obj = paginator.page(1)
+    except EmptyPage:
+        page_obj = paginator.page(paginator.num_pages)
+
     return render(request, 'faktur/items.html', {
-        'query': query,
-        'items': items, 
+        "page_obj": page_obj
     })
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    'A:\\proyek\\fp000\\core\\templates\\core',
+    'A:\\proyek\\fp000\\faktur\\templates\\faktur',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
